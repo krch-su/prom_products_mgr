7ef69cfd0eac46b4653b56b63a198e2e0916050f
krch-sudo <krv93200@proton.me>
2024-01-06 11:30:37 +0200

    serving file

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,29 @@
 import logging
+import threading
 from datetime import datetime
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+
 import schedule
 import requests
 import time
 
 from lxml import objectify, etree
+
+
+class XMLHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Specify the path to your dynamically generated XML file
+        xml_file_path = 'result.xml'
+
+        # Set the content type to XML
+        self.send_response(200)
+        self.send_header('Content-type', 'application/xml')
+        self.end_headers()
+
+        # Open and send the content of the XML file
+        with open(xml_file_path, 'rb') as f:
+            self.wfile.write(f.read())
 
 
 def prepare_file(file_path):
@@ -45,60 +64,97 @@ def prepare_file(file_path):
 # prepare_file('result.xml')
 
 
-def import_file(api_token, file_path):
-    # API endpoint for importing a file
-    endpoint = "https://my.prom.ua/api/v1/products/import_file"
-
-    # Prepare headers with authorization token
-    headers = {"Authorization": f"Bearer {api_token}"}
-
-    # Prepare the request payload
-    files = {"file": open(file_path, "rb")}
-    data = {
-      "url": "string",
-      "force_update": False,
-      "only_available": True,
-      "mark_missing_product_as": "none",
-      "updated_fields": [
-        "price",
-        "presence",
-        "quantity_in_stock",
-        "discount"
-      ]
-    }
-
-    # Make the request
-    response = requests.post(endpoint, headers=headers, files=files, data=data)
-
-    # Check the response
-    if response.status_code == 200:
-        result = response.json()
-        logging.info("Import process ID:", result["id"])
-    else:
-        response.raise_for_status()
+# def import_file(api_token, file_path):
+#     # API endpoint for importing a file
+#     endpoint = "https://my.prom.ua/api/v1/products/import_file"
+#
+#     # Prepare headers with authorization token
+#     headers = {"Authorization": f"Bearer {api_token}"}
+#
+#     # Prepare the request payload
+#     files = {"file": open(file_path, "rb")}
+#     data = {
+#       "force_update": False,
+#       "only_available": True,
+#       "mark_missing_product_as": "none",
+#       "updated_fields": [
+#         "price",
+#         "presence",
+#         "quantity_in_stock",
+#         "discount"
+#       ]
+#     }
+#
+#     # Make the request
+#     response = requests.post(endpoint, headers=headers, files=files, data=data)
+#
+#     # Check the response
+#     if response.status_code == 200:
+#         result = response.json()
+#         logging.info("Import process ID:", result["id"])
+#     else:
+#         response.raise_for_status()
 
 
 def job():
-    print(datetime.now(), 'IMPORT STARTED')
+    print(datetime.now(), 'PREPARING FILE')
     # Replace 'your_api_token' with your actual API token
-    api_token = "0b3ef1abc21e3846c848b150a8c4d05244d9b8d0"
+    # api_token = "0b3ef1abc21e3846c848b150a8c4d05244d9b8d0"
 
     # Replace 'path/to/your/file.xml' with the actual path to your XML file
     file_path = "result.xml"
     try:
         prepare_file(file_path)
         # Call the function to import the file
-        import_file(api_token, file_path)
+        # import_file(api_token, file_path)
     except Exception as ex:
         logging.exception(ex)
 
 
-# # Schedule the job to run every 4 hours
-schedule.every(4).hours.do(job)
+# # # Schedule the job to run every 4 hours
+# schedule.every(4).hours.do(job)
+#
+# # Run the scheduler
+# while True:
+#     logging.info('Scheduler started')
+#     schedule.run_pending()
+#     time.sleep(1)
+#
 
-# Run the scheduler
-while True:
-    logging.info('Scheduler started')
-    schedule.run_pending()
-    time.sleep(1)
+def run_server():
+    # Start the HTTP server
+    host = ''
+    port = 8080
+    server_address = (host, port)
+    httpd = TCPServer(server_address, XMLHandler)
 
+    print(f"Serving XML file at http://{host}:{port}")
+
+    try:
+        # Run the HTTP server in a separate thread
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        # Handle KeyboardInterrupt to gracefully shut down the server
+        print("\nServer stopped.")
+        httpd.shutdown()
+
+
+if __name__ == '__main__':
+    # Schedule the job to run every hour
+    schedule.every().hour.do(job)
+
+    # Start the HTTP server in a separate thread
+    server_thread = threading.Thread(target=run_server)
+    server_thread.start()
+
+    try:
+        while True:
+            # Run scheduled jobs
+            schedule.run_pending()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # Handle KeyboardInterrupt to gracefully shut down the scheduled jobs
+        print("\nScheduled jobs stopped.")
+
+    # Wait for the server thread to finish
+    server_thread.join()

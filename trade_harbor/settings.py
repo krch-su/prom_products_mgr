60ff17aaee593c7ab94c91c368464d28b48c9f0e
krch-sudo <krv93200@proton.me>
2024-02-17 14:22:14 +0200

    fix static collection

--- trade_harbor/settings.py
+++ trade_harbor/settings.py
@@ -115,12 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static/vendor"
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/custom/",
+    BASE_DIR / "static/project/",
     # "/var/www/static/",
 ]
 

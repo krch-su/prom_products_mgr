5b6dd567c81d46f9df35d180fa32652b8f99217e
krch-sudo <krv93200@proton.me>
2024-02-17 13:09:11 +0200

    fix static collection

--- compose/prod/django/start.sh
+++ compose/prod/django/start.sh
@@ -5,5 +5,5 @@ set -o pipefail
 set -o nounset
 
 python manage.py migrate
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
 gunicorn trade_harbor.wsgi:application --bind 0.0.0.0:8000

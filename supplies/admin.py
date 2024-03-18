22ab4f075398f5b6f0570659fb8d61ef766af79b
krch-sudo <krv93200@proton.me>
2024-03-18 11:59:14 +0200

    [feat] bulk price multiplier change from admin

--- supplies/admin.py
+++ supplies/admin.py
@@ -3,12 +3,17 @@ import logging
 import xml.etree.ElementTree as ET
 
 import requests
+from _decimal import Decimal
 from django import forms
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.admin import SimpleListFilter, RelatedFieldListFilter
+from django.contrib.admin.helpers import ActionForm
 from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.contrib.messages import SUCCESS, ERROR
+from django.core.handlers.wsgi import WSGIRequest
 from django.db.models import Func, IntegerField, F
 from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import render
 from django.urls import path, reverse
 from django.utils.html import format_html
 from rangefilter.filters import NumericRangeFilterBuilder
@@ -115,6 +120,11 @@ class OfferForm(forms.ModelForm):
         )
 
 
+class PriceMultiplierForm(ActionForm):
+    action = forms.CharField(initial='set_multiplier')
+    multiplier = forms.DecimalField()
+
+
 @admin.register(models.Offer)
 class OfferAdmin(admin.ModelAdmin):
     form = OfferForm
@@ -158,6 +168,7 @@ class OfferAdmin(admin.ModelAdmin):
         'translate',
         'add_border',
         'add_infographics',
+        'set_multiplier'
     ]
 
     def get_queryset(self, request):
@@ -226,6 +237,25 @@ class OfferAdmin(admin.ModelAdmin):
     def translate(self, request, queryset):
         for offer in queryset:
             translate_offer.delay(offer.pk)
+
+    @admin.action(description="Set Multiplier")
+    def set_multiplier(self, request: WSGIRequest, queryset):
+        if 'apply' in request.POST:
+            form = PriceMultiplierForm(request.POST)
+            logger.debug('APPLY IN POST')
+            if form.is_valid():
+                multiplier = form.cleaned_data['multiplier']
+                queryset.update(price_multiplier=multiplier)
+                messages.add_message(request, SUCCESS, f'Price multiplier updated')
+                return HttpResponseRedirect(request.get_full_path())
+        else:
+            form = PriceMultiplierForm()
+
+        return render(request, 'admin/supplies/offer/set_multiplier_confirmation.html', {
+            'items': queryset.order_by('pk'),
+            'form': form,
+            'title': u'Your title'
+        })
 
     def autocomplete_keyphrase(self, request):
         term = request.GET.get('term')
